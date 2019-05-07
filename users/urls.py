c026699152bfb8f34b49b224e70fc3f6744dea37
PopovaPolina <ppopova.wis@gmail.com>
2019-05-07 21:53:24 +0300

    Minor fix

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 from .views import sign_up, sign_in, ProfileCreateView, DashboardView, CareerView, AdminProfileView
+from dashboards.views import search_by_major, search_by_company, search_by_name 
 
 urlpatterns = [
     url(r'^signup/', sign_up, name='signup'),
@@ -11,4 +12,7 @@ urlpatterns = [
     url(r'^profile/add-career', CareerView.as_view(), name='add-career'),
     url(r'^profile/admin', AdminProfileView.as_view(), name='admin-profile'),
     url(r'^profile', ProfileCreateView.as_view(), name='profile'),
+    url(r'^major-search', search_by_major, name='major-search'),
+    url(r'^company-search', search_by_company, name='company-search'),
+    url(r'^name-search', search_by_name, name='name-search'),
 ]
